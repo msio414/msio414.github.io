d925a967f9abf38db18a1fa29b9d32fa21e71a89
msio808 <muhammedasm00@gmail.com>
2025-02-16 16:31:47 +0300

    commit

---
+++ static/js/toggler.js
@@ -0,0 +1,29 @@
+"use strict";
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+
+let currentTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+
+const updateTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    themeIcon.className = (theme === "dark") ? "fa-solid fa-toggle-on fa-xl" : "fa-solid fa-toggle-off fa-xl";
+    //themeIcon.classList.toggle(icon);
+};
+
+updateTheme(currentTheme);
+document.documentElement.style.visibility = 'visible'; // Remove FOUC guard
+
+themeToggle.addEventListener('click', () => {
+    currentTheme = currentTheme === 'light' ? 'dark' : 'light';
+    updateTheme(currentTheme);
+    localStorage.setItem('theme', currentTheme);
+});
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({ matches }) => {
+    if (!localStorage.getItem('theme')) {
+        currentTheme = matches ? 'dark' : 'light';
+        updateTheme(currentTheme);
+    }
+});
+
+/* "use strict";const themeIcon=document.getElementById('theme-icon');const themeToggle=document.getElementById('theme-toggle');let currentTheme=localStorage.getItem('theme')||(window.matchMedia('(prefers-color-scheme: dark)').matches?'dark':'light');const updateTheme=(theme)=>{document.documentElement.setAttribute('data-theme',theme);const icon=theme==="dark"?"moon":"sun";const iconPath="fonts/sprites.svg#"+icon;themeIcon.querySelector("use").setAttribute("href",iconPath)};updateTheme(currentTheme);document.documentElement.style.visibility='visible';themeToggle.addEventListener('click',()=>{currentTheme=currentTheme==='light'?'dark':'light';updateTheme(currentTheme);localStorage.setItem('theme',currentTheme)});window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change',({matches})=>{if(!localStorage.getItem('theme')){currentTheme=matches?'dark':'light';updateTheme(currentTheme)}}) */
